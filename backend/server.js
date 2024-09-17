dc709d469a6cd25ef8a5cd0905402d1d41a2fdce
fasalu <fasalu@Debian.fasalu>
2024-09-17 10:54:52 +0530

    luxury-ecommerce project

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+
+dotenv.config();
+
+connectDB()
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server runnig on port ${PORT}`);
+});
