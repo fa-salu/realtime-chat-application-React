540ef0a6e60ca620048c4ecc59f144b86ee0ee27
fa-salu <fasalurahman7515@gmail.com>
2024-11-23 21:33:21 +0530

    update url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,13 @@ import "font-awesome/css/font-awesome.css";
 import Main from "./components/Main";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000")
+const socket = io("https://realtime-chatapp-server-41pw.onrender.com");
 
 const App = () => {
-
-
   return (
-   <div>
-    <Main socket={socket} />
-   </div>
+    <div>
+      <Main socket={socket} />
+    </div>
   );
 };
 
