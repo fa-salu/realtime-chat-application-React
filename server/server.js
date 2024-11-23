176c1a044b233ea85e673617b9bbb2514a567086
fa-salu <fasalurahman7515@gmail.com>
2024-11-23 20:57:37 +0530

    url updated

--- server/server.js
+++ server/server.js
@@ -5,11 +5,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-
 const httpServer = createServer();
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
   },
 });
